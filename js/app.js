2fb32bc442106a9f4cc712bcc6eab7092bedbfce
et-nat1995 <tamirnatanov@Tamirs-MacBook-Pro-2.local>
2018-10-22 11:45:54 -0700

    Index Cleaned up Added styles to fixes app.js updated and dried up init.js emptied to original

--- js/app.js
+++ js/app.js
@@ -10,9 +10,134 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var apiKey = "777df2480edd4e6fb87cb0ce9a5ba5bb"
-var queryURL = "https://newsapi.org/v2/everything?sources=ign&q=overwatch&pageSize=3&apiKey=777df2480edd4e6fb87cb0ce9a5ba5bb"
+database.ref().orderByChild("dateAdded").limitToLast(5).on("child_added", function (snapshot) {
+    createButtons(snapshot.val().searchTerm);
+});
 
+
+$("#banner").on("submit", function (e) {
+    e.preventDefault();
+    $(".intro").removeClass("shown");
+    $(".intro").addClass("hide");
+    console.log("submitted!");
+
+    var searchTerm = $("#gameNameSearch").val();
+
+    database.ref().push({
+        searchTerm: searchTerm
+    });
+
+    makeThingsDryYo(searchTerm);
+
+});
+
+$("#find-button").on("click", function (event) {
+    event.preventDefault();
+
+    $(".intro").removeClass("shown");
+    $(".intro").addClass("hide");
+
+    var searchTerm = $("#gameNameSearch").val();
+    console.log(searchTerm);
+
+    database.ref().push({
+        searchTerm: searchTerm
+    });
+
+    makeThingsDryYo(searchTerm);
+
+});
+
+$(document).on("click", ".btn-search", function () {
+
+    $(".intro").removeClass("shown");
+    $(".intro").addClass("hide");
+
+})
+
+$(document).on("click", ".searchTerm", function (event) {
+
+    event.preventDefault();
+
+    $(".intro").removeClass("shown");
+    $(".intro").addClass("hide");
+
+    console.log($(this).val());
+    var searchTerm = $(this).val();
+
+    makeThingsDryYo(searchTerm);
+});
+
+function renderGameInfo(response) {
+
+    $(".articleTitle").text(response.title);
+    $(".publishDate").text(response.publishedAt);
+    $(".articleAuthor").text(response.author);
+    $(".actualSnippet").text(response.description);
+
+    $(".linkButton").attr("href", response.url);
+    $(".linkButton").attr("target", "_blank");
+}
+
+
+
+function createDiv(response, index) {
+
+    var $renderSpot = $(".putArticlesHere");
+    var $newDiv = $("<div class='row row-artist containerImg " + index + "' >");
+
+    var divCol = $("<div class='col-md-4' style='padding-left: 0px;  padding-right: 0px;'>");
+    var newImage = $("<img>");
+    newImage.attr("src", response.urlToImage);
+    console.log(response.urlToImage)
+    newImage.attr("id", "articleImage");
+    newImage.attr("class", "img-responsive");
+
+    divCol.append(newImage);
+    $newDiv.append(divCol);
+
+    var divRow = $("<div class='textColumn col-md-8'>");
+    var divPageHeader = $("<div class=page-header'>");
+    var h3FrirstChild = $("<h3 class='newsTitle'>");
+    var h3SpanFirstChild = $("<span class='articleTitle'>");
+
+    h3FrirstChild.append(h3SpanFirstChild);
+    divPageHeader.append(h3FrirstChild);
+    divRow.append(divPageHeader);
+
+    h3SpanFirstChild.text(response.title);
+
+    divRow.append("<h4> Publish Date: <span class='publishDate'>" +response.publishedAt);
+    divRow.append("<h4>Article Author: <span class='articleAuthor'>"+response.author);
+    divRow.append("<h4>Description:");
+    divRow.append("<p class='actualSnippet'>" + response.description);
+    divRow.append("<a class='btn btn-primary linkButton' href='"+ response.url +"' target='_blank' role='button'>Continue Reading");
+
+    divRow.append("<hr>")
+
+
+    $newDiv.append(divRow);
+    $renderSpot.prepend($newDiv);
+
+}
+
+function makeThingsDryYo(searchTerm) {
+    var queryURL = "https://newsapi.org/v2/everything?sources=ign&q=" + searchTerm + "&pageSize=3&apiKey=777df2480edd4e6fb87cb0ce9a5ba5bb";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+
+        $(".putArticlesHere").empty();
+
+        for (var i = 0; i < response.articles.length; i++) {
+            createDiv(response.articles[i], i);
+        }
+
+    });
+}
 
 function createButtons(value) {
     var $divSpot = $("#buttonRender");
@@ -21,117 +146,6 @@ function createButtons(value) {
 
     $button.attr("href", "#");
     $button.attr("id", "searchTerm");
-    $button.attr("class", "btn-large waves-effect waves-light teal lighten-1");
-    $button.attr("value", value);
-    $button.text(value);
-
-    $divSpot.append($button);
-
-}
-
-database.ref().orderByChild("dateAdded").limitToLast(5).on("child_added", function (snapshot) {
-    createButtons(snapshot.val().searchTerm);
-});
-
-
-$("#banner").on("submit", function (e) {
-    e.preventDefault();
-    $(".row-artist").removeClass("hide");
-    console.log("submitted!");
-
-    var searchTerm = $("#gameNameSearch").val();
-
-
-    var queryURL = "https://newsapi.org/v2/everything?sources=ign&q=" + searchTerm + "&pageSize=3&apiKey=777df2480edd4e6fb87cb0ce9a5ba5bb";
-
-    database.ref().push({
-        searchTerm: searchTerm
-    });
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response)
-        var i;
-        for (i = 0; i < response.articles.length; i++)
-            renderGameInfo(response.articles[i]);
-
-    })
-
-});
-
-$("#find-button").on("click", function (event) {
-    event.preventDefault();
-
-    var searchTerm = $("#gameNameSearch").val();
-    console.log(searchTerm);
-
-    var queryURL = "https://newsapi.org/v2/everything?sources=ign&q=" + searchTerm + "&pageSize=3&apiKey=777df2480edd4e6fb87cb0ce9a5ba5bb";
-
-    database.ref().push({
-        searchTerm: searchTerm
-    });
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response)
-        var i;
-        for (i = 0; i < response.articles.length; i++)
-            renderGameInfo(response.articles[i]);
-
-    })
-   
-
-
-});
-
-$(document).on("click", ".btn-search", function () {
-    $(".row-artist").removeClass("hide");
-})
-
-$(document).on("click", ".searchTerm", function (event) {
-
-    event.preventDefault();
-
-    console.log($(this).val());
-    var searchTerm = $(this).val();
-    var queryURL = "https://newsapi.org/v2/everything?sources=ign&q=" + searchTerm + "&pageSize=3&apiKey=777df2480edd4e6fb87cb0ce9a5ba5bb";
-
-    // populate screen
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response)
-        var i;
-        for (i = 0; i < response.articles.length; i++)
-            renderGameInfo(response.articles[i]);
-
-    })
-});
-
-function renderGameInfo(response) {
-    console.log(response.description);
-
-    $("#articleTitle").text(response.title);
-    $("#publishDate").text(response.publishedAt);
-    $("#articleAuthor").text(response.author);
-    $(".actualSnippet").text(response.description);
-    $(".img-responsive").attr("src", response.urlToImage);
-
-    $(".linkButton").attr("href", response.url);
-    $(".linkButton").attr("target", "_blank");
-}
-
-function createButtons(value) {
-    var $divSpot = $("#buttonRender");
-
-    var $button = $("<button>");
-
-    $button.attr("href", "#");
     $button.attr("class", "btn-large waves-effect waves-light teal lighten-1 searchTerm btn-search");
     $button.attr("value", value);
     $button.text(value);
